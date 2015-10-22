b6b1f043b109cd29f5bdd68fab6345fbb74afa9e
inzl <zliobaite@gmail.com>
2015-10-22 21:49:29 +0300

    salary data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' salaries
+#'
+#' Salaries of professors at a US college.
+#'
+#' @format A data frame with 52 observations and five variables:
+#' \describe{
+#' \item{\code{sex}}{gender: 1 - female, 0 - male}
+#' \item{\code{rank}}{professor rank: 1 - assistant, 2 - associate, 3 - full}
+#' \item{\code{degree}}{degree: 0 - masters, 1 - doctorate}
+#' \item{\code{yeardegree}}{years since degree}
+#' \item{\code{salary}}{salary in USD for 9 months}
+#' }
+#'
+#' Obtained from \url{http://data.princeton.edu/wws509/datasets/\#salary}
+#'
+"salaries"
